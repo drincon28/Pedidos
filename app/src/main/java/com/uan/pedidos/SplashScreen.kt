736dce2f1bf015f397264deaa8616aaa0bf91467
Diana <Rincon>
2020-10-05 22:28:43 -0500

    Initial commit

---
+++ app/src/main/java/com/uan/pedidos/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.uan.pedidos
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen : AppCompatActivity() {
+    private val splashDur = 4000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT*/
+        setContentView(R.layout.splash_screen)
+        Looper.myLooper()?.let {
+            Handler(it).postDelayed({
+                val intent = Intent(this, ActivityDashboard::class.java)
+                startActivity(intent)
+                finish()
+            }, splashDur.toLong())
+        }
+    }
+}
