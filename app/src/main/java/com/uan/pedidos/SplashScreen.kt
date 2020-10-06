be4708380f8ddd1468b43e901b26345e4caa2302
Diana <Rincon>
2020-10-06 00:15:57 -0500

    login, resistro,pedidos

--- app/src/main/java/com/uan/pedidos/SplashScreen.kt
+++ app/src/main/java/com/uan/pedidos/SplashScreen.kt
@@ -6,16 +6,14 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
-
 class SplashScreen : AppCompatActivity() {
     private val splashDur = 4000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT*/
         setContentView(R.layout.splash_screen)
         Looper.myLooper()?.let {
             Handler(it).postDelayed({
-                val intent = Intent(this, MainActivity::class.java)
+                val intent = Intent(this, ActivityDashboard::class.java)
                 startActivity(intent)
                 finish()
             }, splashDur.toLong())
