717222ae4533738db463e6beb98c8fc7256d1716
drincon28 <drincon28@uan.edu.co>
2020-11-01 19:57:07 -0500

    Version 2.1

--- app/src/main/java/com/uan/pedidos/ActivityDashboard.kt
+++ app/src/main/java/com/uan/pedidos/ActivityDashboard.kt
@@ -2,8 +2,6 @@ package com.uan.pedidos
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -24,18 +22,13 @@ class ActivityDashboard : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navView: NavigationView = findViewById(R.id.nav_view_side)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_login, R.id.nav_signup, R.id.nav_order_hist), drawerLayout)
+            R.id.nav_login, R.id.nav_signup, R.id.nav_order_hist), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
